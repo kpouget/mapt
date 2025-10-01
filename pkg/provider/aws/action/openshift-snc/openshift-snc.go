0bc90b31f148f92ff55fd492a9b0e6140aa9f662
Kevin Pouget <kpouget@redhat.com>
2025-10-01 10:21:34 +0200

    pkg/provider/aws/action/openshift-snc/openshift-snc: allow using custom AMI image

--- pkg/provider/aws/action/openshift-snc/openshift-snc.go
+++ pkg/provider/aws/action/openshift-snc/openshift-snc.go
@@ -163,9 +163,11 @@ func (r *openshiftSNCRequest) deploy(ctx *pulumi.Context) error {
 	}
 	// Get AMI
 	amiID := os.Getenv("CUSTOM_AMI_ID")
+	var ami *ec2.LookupAmiResult
+	var err error
 	if amiID == "" {
 		logging.Info("CUSTOM_AMI_ID not set, searching for the right AMI in AWS ...")
-		ami, err := amiSVC.GetAMIByName(ctx,
+		ami, err = amiSVC.GetAMIByName(ctx,
 			fmt.Sprintf("%s*", amiName(r.version, r.arch)),
 			[]string{"self", amiOwner},
 			map[string]string{
@@ -173,11 +175,13 @@ func (r *openshiftSNCRequest) deploy(ctx *pulumi.Context) error {
 		if err != nil {
 			return err
 		}
+	} else {
+		ami = &ec2.LookupAmiResult{
+			Id: amiID, // Your custom AMI ID
+		}
 	}
 	logging.Infof("Using AMI %s", amiID)
-	ami := &ec2.LookupAmiResult{
-		Id: amiID, // Your custom AMI ID
-	}
+
 	nw, err := network.Create(ctx, r.mCtx,
 		&network.NetworkArgs{
 			Prefix:             *r.prefix,
